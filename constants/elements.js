04e219c3bd1a9695e13ffd0eb386ee48afd42fa5
Victor Quiroz <victorhqc@gmail.com>
2017-11-16 12:58:34 +0100

    Improve badges

--- constants/elements.js
+++ constants/elements.js
@@ -2,3 +2,5 @@
 
 export const NPM_LIBRARY_DESCRIPTION = 'npm-library-description';
 export const BADGE = `${NPM_LIBRARY_DESCRIPTION}-badge`;
+export const BUBBLE = 'bubble';
+export const DATE_FORMAT = 'MMMM Do, YYYY ';
