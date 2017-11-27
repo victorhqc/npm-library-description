64272993f776f3d177c8008b54b854f8aa960a11
Victor Quiroz <victorhqc@gmail.com>
2017-11-27 15:17:30 +0100

    Test `addDependency` action

---
+++ spec/store/actions/dependencies-spec.js
@@ -0,0 +1,150 @@
+'use babel';
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+import {
+  addDependency,
+} from '../../../lib/store/actions/dependencies';
+
+import {
+  ADD_DEPENDENCY,
+} from '../../../lib/store/constants/dependencies';
+
+const mockFetch = new MockAdapter(axios);
+const middlewares = [thunk, promiseMiddleware()];
+const mockStore = configureMockStore(middlewares);
+
+fdescribe('addDependency', () => {
+  afterEach(() => {
+    mockFetch.reset();
+  });
+
+  it('Should return a dependency information', () => {
+    const response = {
+      name: 'foo',
+      version: '0.0.0',
+      foo: 'bar',
+    };
+
+    mockFetch.onGet('https://registry.npmjs.org/foo').replyOnce(200, response);
+
+    const expectedActions = [
+      {
+        type: `${ADD_DEPENDENCY}_PENDING`,
+        payload: {
+          line: 10,
+          name: 'foo',
+          textEditorKey: 'some/package.json',
+        },
+      },
+      {
+        type: `${ADD_DEPENDENCY}_FULFILLED`,
+        payload: {
+          line: 10,
+          name: 'foo',
+          textEditorKey: 'some/package.json',
+          data: response,
+        },
+      },
+    ];
+
+    const { dispatch, getActions } = mockStore({});
+
+    waitsForPromise(() => dispatch(addDependency({
+      textEditorKey: 'some/package.json',
+      line: 10,
+      name: 'foo',
+    })).then(() => {
+      expect(
+        getActions(),
+      ).toEqual(expectedActions);
+    }));
+  });
+
+  it('Should avoid calling `npm` API if it was already fetched but it should update line', () => {
+    const expectedActions = [
+      {
+        type: `${ADD_DEPENDENCY}_PENDING`,
+        payload: {
+          line: 10,
+          name: 'foo',
+          textEditorKey: 'some/package.json',
+        },
+      },
+      {
+        type: `${ADD_DEPENDENCY}_FULFILLED`,
+        payload: {
+          line: 10,
+          name: 'foo',
+          data: {
+            something: 'old',
+          },
+        },
+      },
+    ];
+
+    const { dispatch, getActions } = mockStore({
+      dependencies: {
+        'some/package.json': {
+          foo: {
+            name: 'foo',
+            line: 9,
+            data: {
+              something: 'old',
+            },
+          },
+        },
+      },
+    });
+
+    waitsForPromise(() => dispatch(addDependency({
+      textEditorKey: 'some/package.json',
+      line: 10,
+      name: 'foo',
+    })).then(() => {
+      expect(
+        getActions(),
+      ).toEqual(expectedActions);
+    }));
+  });
+
+  it('Should handle errors', () => {
+    const response = {
+      error: 'Something went wrong',
+    };
+
+    mockFetch.onGet('https://registry.npmjs.org/foo').replyOnce(500, response);
+
+    const expectedActions = [
+      {
+        type: `${ADD_DEPENDENCY}_PENDING`,
+        payload: {
+          line: 10,
+          name: 'foo',
+          textEditorKey: 'some/package.json',
+        },
+      },
+      {
+        type: `${ADD_DEPENDENCY}_REJECTED`,
+        payload: new Error('Problems getting dependency'),
+        error: true,
+      },
+    ];
+
+    const { dispatch, getActions } = mockStore({});
+
+    waitsForPromise(() => dispatch(addDependency({
+      textEditorKey: 'some/package.json',
+      line: 10,
+      name: 'foo',
+    })).catch(() => {
+      expect(
+        getActions(),
+      ).toEqual(expectedActions);
+    }));
+  });
+});
